ff8da4a3181b21343513180682dfbc29862cebd0
Nick Zimmerman <n.zman@eaglecom.net>
2021-09-17 20:28:12 -0500

    Assignment 2

---
+++ main.js
@@ -0,0 +1,42 @@
+//Question 1
+let myNumber;
+
+//Question 2
+myNumber = 5;
+
+//Question 3
+let myOtherNumber;
+myOtherNumber = 5;
+
+//Question 4
+let myTotal;
+myTotal = myNumber * myOtherNumber;
+
+//Question 5
+let myName = "";
+let myCombo = "";
+myCombo = String(myNumber + myOtherNumber);
+
+console.log(typeof(myCombo));
+
+//Question 6
+let myOtherCombo;
+myOtherCombo = myNumber + myOtherNumber;
+
+console.log(typeof(myOtherCombo));
+
+//Question 7
+let myRemainder;
+myRemainder = myNumber % 5;
+
+//Question 8
+let i;
+i = 2;
+
+//Question 9
+let int;
+int = 4;
+
+//Question 10
+let myBoolean;
+myBoolean = false;
